0f4cf0b0aef73da5d27253f921656b4a355bef3f
marc1107 <m.bohner99@gmail.com>
2024-10-15 08:26:22 +0200

    renamed HomeController to ReversiController

--- app/controllers/ReversiController.scala
+++ app/controllers/ReversiController.scala
@@ -11,7 +11,7 @@ import de.htwg.model.Move
  * application's home page.
  */
 @Singleton
-class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+class ReversiController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
 
   /**
    * Create an Action to render an HTML page.
