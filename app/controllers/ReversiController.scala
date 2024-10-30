d8a508719a4bfefc813d92da941c4362811d0184
marc1107 <m.bohner99@gmail.com>
2024-10-30 20:32:25 +0100

    implemented changing fields using jquery and ajax

--- app/controllers/ReversiController.scala
+++ app/controllers/ReversiController.scala
@@ -1,10 +1,12 @@
 package controllers
 
-import javax.inject._
-import play.api._
-import play.api.mvc._
+import javax.inject.*
+import play.api.*
+import play.api.mvc.*
 import de.htwg.Reversi
-import de.htwg.model.Move
+import de.htwg.model.{Move, Stone}
+import de.htwg.model.fieldComponent.FieldInterface
+import play.api.libs.json.{JsValue, Json, Writes}
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
@@ -22,6 +24,12 @@ class ReversiController @Inject()(val controllerComponents: ControllerComponents
    */
 
   private val gameController = Reversi.controller
+
+  implicit val stoneWrites: Writes[Stone] = Writes[Stone] {
+    case Stone.Empty => Json.toJson("EMPTY")
+    case Stone.B => Json.toJson("B")
+    case Stone.W => Json.toJson("W")
+  }
 
   def index(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.index())
@@ -56,8 +64,34 @@ class ReversiController @Inject()(val controllerComponents: ControllerComponents
     Ok(views.html.game(gameController.field, gameController.playerState.getStone))
   }
 
+  def makeMoveAjax(row: Int, col: Int): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
+    val oldBoard = gameController.field
+    val oldBoardJson = fieldToJson(oldBoard)
+    doMove(row, col)
+    val updatedBoard = gameController.field
+    val boardJson = fieldToJson(updatedBoard)
+    val response = Json.obj(
+      "oldBoard" -> oldBoardJson,
+      "newBoard" -> boardJson
+    )
+    Ok(response)
+  }
+
   def rules(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.rules())
+  }
+
+  private def fieldToJson(field: FieldInterface): JsValue = {
+    val fieldJson = Json.obj(
+      "size" -> field.size,
+      "cells" -> (0 until field.size).map { row =>
+        (0 until field.size).map { col =>
+          Json.toJson(field.get(row + 1, col + 1))
+        }
+      },
+      "playerState" -> Json.toJson(gameController.playerState.getStone)
+    )
+    fieldJson
   }
 
   private def doMove(row: Int, column: Int): Unit = gameController.doAndPublish(gameController.put, Move(gameController.playerState.getStone, row, column))
---
+++ public/javascripts/main.js
@@ -0,0 +1,49 @@
+function makeMove(row, col) {
+    console.log("Move made at row: " + row + ", column: " + col);
+    $.ajax({
+        url: `/makeMoveAjax/${row}/${col}`,
+        method: 'GET',
+        success: function(response) {
+            updateBoard(response);
+        },
+        error: function(xhr, status, error) {
+            console.error("Error making move:", error);
+        }
+    });
+}
+
+function updateBoard(response) {
+    const oldBoard = response.oldBoard.cells;
+    const updatedBoard = response.newBoard.cells;
+    const size = response.newBoard.size;
+    const currentPlayer = response.newBoard.playerState;
+
+    // Update the board cells
+    for (let row = 0; row < size; row++) {
+        for (let col = 0; col < size; col++) {
+            const cell = $(`td[data-row='${row + 1}'][data-cell='${col + 1}']`);
+            const oldStone = oldBoard[row][col];
+            const newStone = updatedBoard[row][col];
+            let content;
+
+            if (oldStone === "B" && newStone === "W") {
+                content = "<div class='stone black flip-to-white'></div>";
+            } else if (oldStone === "W" && newStone === "B") {
+                content = "<div class='stone white flip-to-black'></div>";
+            } else if (newStone === "B") {
+                content = "<div class='stone black'></div>";
+            } else if (newStone === "W") {
+                content = "<div class='stone white'></div>";
+            } else {
+                content = "&nbsp;";
+            }
+
+            cell.html(content);
+        }
+    }
+
+    // Update the current player display
+    const playerClass = currentPlayer === "B" ? "black" : "white";
+    $("#playerturn1").attr("class", `playerturn ${playerClass}`);
+    $("#playerturn2").attr("class", `playerturn ${playerClass}`);
+}
