f0fe8ff25bb8b19e467886aa5958740da2b5b260
marc1107 <m.bohner99@gmail.com>
2024-10-30 22:48:31 +0100

    added more click sounds and dropdown to select click sounds

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,5 @@
+let selectedSound = 'click-sound-click1';
+
 function makeMove(row, col) {
     console.log("Move made at row: " + row + ", column: " + col);
     $.ajax({
@@ -5,7 +7,6 @@ function makeMove(row, col) {
         method: 'GET',
         success: function(response) {
             updateBoard(response);
-            playClickSound();
         },
         error: function(xhr, status, error) {
             console.error("Error making move:", error);
@@ -19,6 +20,8 @@ function updateBoard(response) {
     const size = response.newBoard.size;
     const currentPlayer = response.newBoard.playerState;
 
+    let changed = false;
+
     // Update the board cells
     for (let row = 0; row < size; row++) {
         for (let col = 0; col < size; col++) {
@@ -28,8 +31,10 @@ function updateBoard(response) {
             let content;
 
             if (oldStone === "B" && newStone === "W") {
+                changed = true;
                 content = "<div class='stone black flip-to-white'></div>";
             } else if (oldStone === "W" && newStone === "B") {
+                changed = true;
                 content = "<div class='stone white flip-to-black'></div>";
             } else if (newStone === "B") {
                 content = "<div class='stone black'></div>";
@@ -43,13 +48,35 @@ function updateBoard(response) {
         }
     }
 
+    if (!changed) {
+        playErrorSound();
+        return;
+    }
+
+    playClickSound();
+
     // Update the current player display
     const playerClass = currentPlayer === "B" ? "black" : "white";
     $("#playerturn1").attr("class", `playerturn ${playerClass}`);
     $("#playerturn2").attr("class", `playerturn ${playerClass}`);
 }
 
+function changeClickSound() {
+    const selectedValue = document.getElementById('sound-select').value;
+    selectedSound = 'click-sound-' + selectedValue;
+}
+
 function playClickSound() {
-    const clickSound = document.getElementById('click-sound');
+    const clickSound = document.getElementById(selectedSound);
+    clickSound.play();
+}
+
+function playErrorSound() {
+    const errorSound = document.getElementById('error-sound1');
+    errorSound.play();
+}
+
+function playButtonClickSound() {
+    const clickSound = document.getElementById('button-click-sound');
     clickSound.play();
 }
